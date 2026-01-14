09a19bbe7f0d9adaa0cce8dfcac367c969c541ac
25lp6724 User <25lp6724@25lp9041nestedumns-iMac.local>
2026-01-14 12:05:43 +0800

    Module CRUD done

---
+++ apps/TeacherDoc/app/api/module/[id]/route.ts
@@ -0,0 +1,29 @@
+import  prismaClient  from "@/lib/prismaClient";
+import { NextResponse } from "next/server";
+
+const prisma = prismaClient;
+
+export async function DELETE(request: Request, { params }: { params: { id: string } }) {
+try {
+    const {id} = params
+    const deleteModule = await prisma.module.delete({
+        where: {id}
+    })
+    return NextResponse.json(deleteModule, {status: 200});
+} catch (error) {
+    return NextResponse.json({ error: 'Failed to delete module' }, { status: 500 });
+}}
+
+export async function PATCH(request: Request, { params }: { params: { id: string } }) {
+    try {
+        const {id} = params;
+        const body = await request.json();
+    
+        const updateModule = await prisma.module.update({
+            where: {id},
+            data: body
+        })
+        return NextResponse.json(updateModule, {status: 200});
+    } catch (error) {
+        return NextResponse.json({ error: 'Failed to update module' }, { status: 500 });
+    }}
