d3d4bcb6d5dc4e420a343e0639339bccc33b066c
ZiCoL <157025293+Z1CoL@users.noreply.github.com>
2026-01-14 12:34:07 +0800

    subModule page frontend done. Odoo backend route bolchihvol fetch hiiged maplaad buren bolno (#34)

--- apps/TeacherDoc/app/sub-module/[id]/page.tsx
+++ apps/TeacherDoc/app/sub-module/[id]/page.tsx
@@ -1,7 +1,32 @@
+import { LessonCard } from '@/app/_components/lessonCard';
+
 export default function SubModulePage() {
   return (
-    <div>
-      <div>hello</div>
+    <div className="flex flex-col gap-10 h-screen bg-blue-50 justify-start">
+      {/* lesson description  */}
+      <div className=" mt-10 mx-20">
+        <div className="bg-white py-5 border flex flex-col items-start gap-8 w-full px-10 rounded-xl">
+          <h2 className="font-bold text-xl">Хичээлийн тайлбар</h2>
+          <span className="text-xl  ">
+            Магадлалын үзэгдэл, тэдгээрийн төрөл, шинж чанарыг судална.
+          </span>
+        </div>
+      </div>
+
+      {/* lessons */}
+
+      {/* lessons title */}
+      <div className="mx-20 flex flex-col gap-3">
+        <h2 className="font-bold text-xl">Бэлтгэсэн багш нар</h2>
+        <span className="text-xl  ">
+          Энэ хичээлийг бэлтгэсэн багш нарын жагсаалт. Багш дээр дарж материалыг
+          үзнэ үү.
+        </span>
+      </div>
+
+      <div className="mx-20">
+        <LessonCard />
+      </div>
     </div>
   );
 }
