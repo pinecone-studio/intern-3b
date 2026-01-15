890adff00484db1b21e5b3a3afdebcb6e74f979b
urn <ourantogos65@gmail.com>
2026-01-15 12:41:02 +0800

    add school signinform (#49)
    Co-authored-by: 25lp5593 <25lp5593@G303-CC0161.local>

--- apps/TeacherDoc/app/login/page.tsx
+++ apps/TeacherDoc/app/login/page.tsx
@@ -1,9 +1,28 @@
+'use client';
+
+import { useState } from 'react';
 import AuthPage from '../_components/AuthPage';
+import { Button } from '../_components/ui/CustomUI';
 
 export default function LoginPage() {
+  const [role, setRole] = useState<null | 'school' | 'teacher'>(null);
+
   return (
     <div className="min-h-screen bg-[#fafafa] font-sans text-slate-900 selection:bg-[#B0DAC8]/30 flex items-center justify-center">
-      <AuthPage />
+      {!role && (
+        <div className="flex gap-4">
+          <Button variant="primary" onClick={() => setRole('school')}>
+            Сургууль
+          </Button>
+
+          <Button onClick={() => setRole('teacher')}>
+            Багш
+          </Button>
+        </div>
+      )}
+      {role && (
+        <AuthPage role={role} onBack={() => setRole(null)} />
+      )}
     </div>
   );
 }
