e70c072178d0cd36bd62d4ca05effc0f5b2e197b
25lp5593 <25lp5593@G303-CC0161.local>
2026-01-15 12:35:32 +0800

    add school signinform

--- apps/TeacherDoc/app/_components/AuthPage.tsx
+++ apps/TeacherDoc/app/_components/AuthPage.tsx
@@ -1,21 +1,72 @@
 'use client';
 
-import { useState } from 'react';
-import SignInForm from './SignInForm';
-import { SignUpForm } from './SignUpForm';
+import { useState } from "react";
+import { SignUpForm } from "./SignUpForm";
+import SignInForm from "./SignInForm";
 
-export default function AuthPage() {
+import SignInSchoolForm from "./SignInSchoolForm";
+import { SignUpSchoolForm } from "./SignUpSchoolForm";
+
+
+
+type AuthPageProps = {
+  role: 'school' | 'teacher';
+  onBack: () => void;
+};
+
+export default function AuthPage({ role, onBack }: AuthPageProps) {
   const [isSignUp, setIsSignUp] = useState(false);
 
   return (
-    <div className="w-full max-w-md">
-      <div className="bg-white rounded-lg shadow-xl p-8">
-        <div className="mb-8 text-center">
-          <h1 className="text-3xl font-bold text-slate-900 mb-2">
-            {isSignUp ? 'Бүртгүүлэх' : 'Нэвтрэх'}
-          </h1>
-          <p className="text-slate-600">
-            {isSignUp
+    <div className="w-full max-w-md p-6 ">
+   
+  {role === "school" && (
+  <div className="w-full max-w-md">
+       <div className="bg-white rounded-lg shadow-xl p-8">
+         <div className="mb-8 text-center">
+           <h1 className="text-3xl font-bold text-slate-900 mb-2">
+             {isSignUp ? 'Бүртгүүлэх' : 'Нэвтрэх'}
+           </h1>
+           <p className="text-slate-600">
+             {isSignUp
+              ? 'Шинэ хэрэглэгчийн данс үүсгэх'
+              : 'Өөрийн данс руу нэвтрэх'}
+          </p>
+        </div>
+
+        {isSignUp ? < SignUpSchoolForm/> : <SignInSchoolForm />}
+
+        <div className="mt-6 text-center">
+          <button
+            onClick={() => setIsSignUp(!isSignUp)}
+            className="text-sm text-slate-600 hover:text-slate-900 transition-colors"
+          >
+            {isSignUp ? (
+              <>
+                Танд данс байна уу?{' '}
+                <span className="font-semibold text-blue-600">Нэвтрэх</span>
+              </>
+            ) : (
+              <>
+                Шинэ хэрэглэгч үү?{' '}
+                <span className="font-semibold text-blue-600">Бүртгүүлэх</span>
+              </>
+            )}
+          </button>
+        </div>
+      </div>
+    </div>
+)}
+
+   {role === "teacher" &&(
+      <div className="w-full max-w-md">
+       <div className="bg-white rounded-lg shadow-xl p-8">
+         <div className="mb-8 text-center">
+           <h1 className="text-3xl font-bold text-slate-900 mb-2">
+             {isSignUp ? 'Бүртгүүлэх' : 'Нэвтрэх'}
+           </h1>
+           <p className="text-slate-600">
+             {isSignUp
               ? 'Шинэ хэрэглэгчийн данс үүсгэх'
               : 'Өөрийн данс руу нэвтрэх'}
           </p>
@@ -42,6 +93,12 @@ export default function AuthPage() {
           </button>
         </div>
       </div>
+    </div>
+   )}
+
+      <button onClick={onBack} className="mt-4 text-sm text-gray-500">
+        back
+      </button>
     </div>
   );
 }
---
+++ apps/TeacherDoc/app/_components/SignInSchoolForm.tsx
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+import FormInput from './FormInput';
+import FormButton from './FormButton';
+import { waitForDebugger } from 'inspector';
+
+export default function SignInSchoolForm() {
+  const [formData, setFormData] = useState({
+    phoneNumber: '',
+    password: ' ',
+  });
+  const [errors, setErrors] = useState<Record<string, string>>({});
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const newErrors: Record<string, string> = {};
+    if (!formData.phoneNumber.trim()) {
+      newErrors.phoneNumber = 'Утасны дугаар оруулна уу';
+    }
+    if (!formData.password.trim()) {
+      newErrors.password = 'Нууц үг оруулна уу';
+    }
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+      return;
+    }
+    setLoading(true);
+    //Yg ene hesegt db ruu nemdeg logic orj irne e.g axiosInstance
+
+    // await new Promise((resolve) => setTimeout(resolve, 1500));
+    // console.log('Sign up:', formData);
+    // setLoading(false);
+  };
+  const handleChange = (field: string, value: string) => {
+    setFormData((prev) => {
+      return { ...prev, [field]: value };
+    });
+    if (errors[field]) {
+      setErrors((prev) => ({ ...prev, [field]: '' }));
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="space-y-5">
+      <FormInput
+        label="Утасны дугаар"
+        type="tel"
+        value={formData.phoneNumber}
+        onChange={(value) => handleChange('phoneNumber', value)}
+        error={errors.phoneNumber}
+        placeholder="99119911"
+      />
+
+      <FormInput
+        label="Нууц үг"
+        type="password"
+        value={formData.password}
+        onChange={(value) => handleChange('password', value)}
+        error={errors.password}
+        placeholder="••••••••"
+      />
+
+      <FormButton loading={loading}>Нэвтрэх</FormButton>
+    </form>
+  );
+}
