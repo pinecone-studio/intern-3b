5a889fe70990ddd089b6864ca9802a428eb93595
bolormau <bolormau@gmail.com>
2026-01-17 17:41:19 +0800

    unitok/mobile/chore: fix metro, babel config bugs and related files path (#88)

--- apps/unitok-mobile/tailwind.config.js
+++ apps/unitok-mobile/tailwind.config.js
@@ -1,9 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app.{js,jsx,ts,tsx}',
-    './app/**/*.{js,jsx,ts,tsx}',
     './src/**/*.{js,jsx,ts,tsx}',
+    './src/app/**/*.{js,jsx,ts,tsx}',
   ],
   presets: [require('nativewind/preset')],
   theme: {
