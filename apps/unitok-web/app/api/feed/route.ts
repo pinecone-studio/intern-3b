568278cd2ad39b7162870a0f9c3d530ee5986006
bolormau <bolormau@gmail.com>
2026-01-23 06:58:14 +0800

    feat(unitok-web/back-end): modify clerk webhooks route and update prisma user and university schema

--- apps/unitok-web/app/api/feed/route.ts
+++ apps/unitok-web/app/api/feed/route.ts
@@ -1,4 +1,4 @@
-import { getFeed } from '@/apps/unitok-web/services/feed.service';
+import { getFeed } from '@/services/feed.service';
 
 export async function GET() {
   const data = await getFeed();
