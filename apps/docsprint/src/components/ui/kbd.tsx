2823624b67482dc5e75f9ec2094280d91d5f38ce
Spibb20 <151099509+Spibb20@users.noreply.github.com>
2026-06-18 09:44:34 +0800

    docsprint: configure deployment and fix build dependencies (#235)

--- apps/docsprint/src/components/ui/kbd.tsx
+++ apps/docsprint/src/components/ui/kbd.tsx
@@ -1,4 +1,4 @@
-import { cn } from '@intern-3b/shadcn';
+import { cn } from '@/lib/utils';
 
 function Kbd({ className, ...props }: React.ComponentProps<'kbd'>) {
   return (
--- apps/docsprint/src/components/ui/switch.tsx
+++ apps/docsprint/src/components/ui/switch.tsx
@@ -3,7 +3,7 @@
 import * as React from 'react';
 import * as SwitchPrimitive from '@radix-ui/react-switch';
 
-import { cn } from '@intern-3b/shadcn';
+import { cn } from '@/lib/utils';
 
 function Switch({
   className,
